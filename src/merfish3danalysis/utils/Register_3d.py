11455aaddcdad501ef8293b4b69d3f046e12a1a1
amelzd <azidoum@protonmail.com>
2026-06-10 10:29:56 +0200

    Update Register_3d.py

--- src/merfish3danalysis/utils/Register_3d.py
+++ src/merfish3danalysis/utils/Register_3d.py
@@ -222,7 +222,7 @@ def main():
     )
 
     # Saving outputs
-    tiff.imwrite(args.out_moving, np.clip(moving_corr, np.iinfo(orig_dtype_mov).min, np.iinfo(orig_dtype_move).max).astype(orig_dtype_mov) )
+    tiff.imwrite(args.out_moving, np.clip(moving_corr, np.iinfo(orig_dtype_mov).min, np.iinfo(orig_dtype_mov).max).astype(orig_dtype_mov) )
     if tomove_corr is not None and args.out_tomove is not None:
         tiff.imwrite(args.out_tomove, np.clip(tomove_corr, np.iinfo(orig_dtype).min,  np.iinfo(orig_dtype).max,).astype(orig_dtype) )
     np.save(args.out_warp, warp_field)
